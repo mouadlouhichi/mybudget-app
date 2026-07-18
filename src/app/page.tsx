435b230a2c6d64a924bc74eba6204e3c5d5179c2
Mouad Louhichi <mouadlouhichi@gmail.com>
2026-07-18 15:05:47 +0200

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { useAuth } from '@/lib/auth-context'
 
+
 export default function Root() {
   const { user, loading } = useAuth()
   const router = useRouter()
